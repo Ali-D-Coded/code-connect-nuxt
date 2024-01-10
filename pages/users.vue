51e521ad0fc1223a387d0d40e94a876fd9dda210
Ali Afthab <aliallu3xa@gmail.com>
2024-01-10 17:01:27 +0530

    fnrnrej

--- pages/users.vue
+++ pages/users.vue
@@ -1,19 +1,31 @@
 <script lang="ts" setup>
-import { PlusOutlined } from "@ant-design/icons-vue";
+import { PlusOutlined, EyeOutlined } from "@ant-design/icons-vue";
 
 const columns = [
   {
-    title: "Name",
-    dataIndex: "name",
+    title: "First Name",
+    dataIndex: "firstName",
   },
   {
-    title: "Cash Assets",
+    title: "Last Name",
     className: "column-money",
-    dataIndex: "money",
+    dataIndex: "lastName",
   },
   {
-    title: "Address",
-    dataIndex: "address",
+    title: "UserName",
+    dataIndex: "userName",
+  },
+  {
+    title: "Phone",
+    dataIndex: "phone",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
   },
   {
     title: "Actions",
@@ -23,30 +35,58 @@ const columns = [
 
 const data = [
   {
-    key: "1",
-    name: "John Brown",
-    money: "￥300,000.00",
-    address: "New York No. 1 Lake Park",
+    key: 1,
+    firstName: "Hunais",
+    lastName: "Mongan",
+    userName: "mongan",
+    phone: "9865985665",
+    email: "abs@gmail.com",
+    password: "123456789",
+    status: "ACTIVE",
   },
   {
-    key: "2",
-    name: "Jim Green",
-    money: "￥1,256,000.00",
-    address: "London No. 1 Lake Park",
+    key: 2,
+    firstName: "Sufail",
+    lastName: "Akashavani",
+    userName: "kunjapu",
+    phone: "9865985665",
+    email: "abs@gmail.com",
+    password: "123456789",
+    status: "ACTIVE",
   },
   {
-    key: "3",
-    name: "Joe Black",
-    money: "￥120,000.00",
-    address: "Sidney No. 1 Lake Park",
+    key: 3,
+    firstName: "Sreya",
+    lastName: "Sasikumar",
+    userName: "ammu",
+    phone: "9865985665",
+    email: "abs@gmail.com",
+    password: "123456789",
+    status: "ACTIVE",
   },
 ];
 
 const open = useState("openmodal", () => false);
+const showData = useState("showData", () =>
+  shallowRef({
+    key: 3,
+    firstName: "fdvgdf",
+    lastName: "",
+    userName: "",
+    phone: "",
+    email: "",
+    password: "",
+    status: "",
+  })
+);
 
-const showModal = () => {
+const showModal = (record: any) => {
+  showData.value = record;
+
   open.value = true;
 };
+
+console.log(showData.value.firstName);
 </script>
 <template>
   <div>
@@ -57,31 +97,58 @@ const showModal = () => {
       :data-source="data"
       bordered
     >
-      <template #bodyCell="{ column, text }">
+      <template #bodyCell="{ column, record, text }">
         <template v-if="column.dataIndex === 'name'">
           <a class="hover:text-green-300 hover:font-bold">{{ text }}</a>
         </template>
         <template v-if="column.dataIndex === 'actions'">
-          <div class="flex gap-1">
-            <a-button @click="showModal"> Edit </a-button>
-            <a-button> delete </a-button>
+          <div class="flex items-center gap-3">
+            <!-- <a-button  class="p-2"> -->
+            <EyeOutlined @click="showModal(record)" class="text-2xl" />
+            <!-- </a-button> -->
+            <a-button
+              >{{
+                record.status == "ACTIVE"
+                  ? "block"
+                  : record.status == "BLOCKED"
+                  ? "unblock"
+                  : ""
+              }}
+            </a-button>
           </div>
         </template>
       </template>
       <template #title>
         <div class="flex justify-between">
           <div class="text-3xl font-bold">Users</div>
-          <!-- <button class="shadow-md border h-10 w-10 rounded-full pb-1">
+          <!-- <button class="w-10 h-10 pb-1 border rounded-full shadow-md">
           <PlusOutlined />
         </button> -->
         </div>
       </template>
     </a-table>
 
-    <a-modal :footer="false" v-model:open="open" title="Basic Modal">
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+    <a-modal :footer="false" v-model:open="open">
+      <a-descriptions bordered title="User Info" size="small" layout="vertical">
+        <a-descriptions-item label="First Name">
+          {{ showData.firstName }}
+        </a-descriptions-item>
+        <a-descriptions-item label="Last Name">{{
+          showData.lastName
+        }}</a-descriptions-item>
+        <a-descriptions-item label="Username">{{
+          showData.userName
+        }}</a-descriptions-item>
+        <a-descriptions-item label="Phone">{{
+          showData.phone
+        }}</a-descriptions-item>
+        <a-descriptions-item label="Email">{{
+          showData.email
+        }}</a-descriptions-item>
+        <a-descriptions-item label="Status">{{
+          showData.status
+        }}</a-descriptions-item>
+      </a-descriptions>
     </a-modal>
   </div>
 </template>
